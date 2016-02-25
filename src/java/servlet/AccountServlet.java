619e797d04fdb401ddeed6ca69529fec7c23e3d7
c0667381 <c0667381@SB-N208-29.lambton.on.ca>
2016-02-25 17:17:57 -0500

    Account servlet created.

--- src/java/servlet/AccountServlet.java
+++ src/java/servlet/AccountServlet.java
@@ -40,21 +40,21 @@ public class AccountServlet extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setHeader("Cache-Control", "private, no-store, no-cache, must-revalidate");
         response.setHeader("Pragma", "no-cache");
-        response.setDateHeader("Expires",0);
+        response.setDateHeader("Expires", 0);
         double balance = account.getBalance();
-      
+
     }
 
     @Override
-    protected void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException {
-        
-        PrintWriter p = response.getWriter(); {
-            if (request.getParameter("Withdraw")!= null) {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter p = response.getWriter();
+        {
+            if (request.getParameter("Withdraw") != null) {
                 account.withdraw(Double.parseDouble(request.getParameter("Withdraw")));
-            } else if (request.getParameter("Deposit")!= null) {
+            } else if (request.getParameter("Deposit") != null) {
                 account.deposit(Double.parseDouble(request.getParameter("Deposit")));
             } else if ("true".equals(request.getParameter("Close"))) {
                 account.close();
@@ -62,6 +62,6 @@ public class AccountServlet extends HttpServlet {
                 p.println("Error::no parameters");
             }
             doGet(request, response);
-    }
+        }
     }
 }
